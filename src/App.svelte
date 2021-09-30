c6f07b76b55d207128920976375a88ad0588146e
Wilbert <wilbert_evans@hotmail.com>
2021-09-30 17:09:04 +0100

    initial coffee 2 point 0 in svelte

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  import logo from './assets/coffee2point0.png'
+  import Counter from './lib/Counter.svelte'
+  import Coffee from './lib/coffee.svelte'
+</script>
+
+<main>
+  <img src={logo} alt="Svelte Logo" />
+
+  <Coffee />
+
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+
+  img {
+    height: 20rem;
+    width: 29rem;
+  }
+</style>
